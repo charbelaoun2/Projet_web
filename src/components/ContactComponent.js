94bb92e77c369e407ea65da7212fc89124e46e58
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2021-10-16 21:19:28 +0300

    Modification de ContactUs

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -99,20 +99,18 @@ class Contact extends Component {
                     <div className="col-12 col-sm-4 offset-sm-1">
                             <h5>Our Address</h5>
                             <address>
-                            121, Clear Water Bay Road<br />
-                            Clear Water Bay, Kowloon<br />
-                            HONG KONG<br />
-                            <i className="fa fa-phone"></i>: +852 1234 5678<br />
-                            <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            Beirut-Lebanon
+                            Naccach-Lebanon
+                            Tripoli-Lebanon<br />
+                            <i className="fa fa-phone"></i>: +961 01 888 999 <br />
+                            <i className="fa fa-fax"></i>: +961 09 998 889<br />
+                            <i className="fa fa-envelope"></i>: <a href="MCoperations@gmail.com">MCoperations@gmail.com</a>
                             </address>
                     </div>
-                    <div className="col-12 col-sm-6 offset-sm-1">
-                        <h5>Map of our Location</h5>
-                    </div>
+                   
                     <div className="col-12 col-sm-11 offset-sm-1">
                         <div className="btn-group" role="group">
-                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-primary" href="tel:+961 01 888 999"><i className="fa fa-phone"></i> Call</a>
                             <a role="button" className="btn btn-info"> <i className="fa fa-skype"></i> Skype</a>
                             <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
