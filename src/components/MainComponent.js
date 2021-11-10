094be60bc8e0ad66ca44badf7d500fafd2170dc4
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2021-11-10 12:44:43 +0200

    add

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,77 +1,75 @@
-import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import DonationDetail from './DonationDetailComponent';
-import AboutUs from './AboutUsComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import Home from './HomeComponent';
-import Donate from './Donate';
-import Contact from './ContactComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
-import { connect } from 'react-redux';
-import { addComment, fetchDonations } from '../redux/ActionCreators';
-import Login from './Login';
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import DonationDetail from "./DonationDetailComponent";
+import AboutUs from "./AboutUsComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Donate from "./Donate";
+import Contact from "./ContactComponent";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { addComment, fetchDonations } from "../redux/ActionCreators";
+import Login from "./Login";
 
+import { selectUser } from "../redux/userSlice";
+import { useSelector } from "react-redux";
 
-import { selectUser } from '../redux/userSlice';
-import { useSelector } from 'react-redux';
-
-
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     donations: state.donations,
     comments: state.comments,
     promotions: state.promotions,
-    leaders: state.leaders
-  }
-}
-const mapDispatchToProps = dispatch => ({
-
-  addComment: (donationId, rating, author, comment) => 
-      dispatch( addComment(donationId, rating, author, comment)),
-      fetchDonations: () => { dispatch(fetchDonations())}
-})
-
-
-
-
+    leaders: state.leaders,
+  };
+};
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (donationId, rating, author, comment) =>
+    dispatch(addComment(donationId, rating, author, comment)),
+  fetchDonations: () => {
+    dispatch(fetchDonations());
+  },
+});
 
 class Main extends Component {
-
-  
-
-
-
-
   componentDidMount() {
     this.props.fetchDonations();
   }
 
-
-  
   render() {
-
     const HomePage = () => {
-      return(
-          <Home 
-              donation={this.props.donations.donations.filter((donation) => donation.featured)[0]}
-              donationsLoading={this.props.donations.isLoading}
-              donationsErrMess={this.props.donations.errMess}
-              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-          />
+      return (
+        <Home
+          donation={
+            this.props.donations.donations.filter(
+              (donation) => donation.featured
+            )[0]
+          }
+          donationsLoading={this.props.donations.isLoading}
+          donationsErrMess={this.props.donations.errMess}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
-    }
+    };
 
-    const DonationWithId = ({match}) => {
-      return(
-          <DonationDetail donation={this.props.donations.donations.filter((donation) => donation.id === parseInt(match.params.donationId,10))[0]}
-            isLoading={this.props.donations.isLoading}
-            errMess={this.props.donations.errMess}
-            comments={this.props.comments.filter((comment) => comment.donationId === parseInt(match.params.donationId,10))}
-            addComment={this.props.addComment}
-          />
+    const DonationWithId = ({ match }) => {
+      return (
+        <DonationDetail
+          donation={
+            this.props.donations.donations.filter(
+              (donation) =>
+                donation.id === parseInt(match.params.donationId, 10)
+            )[0]
+          }
+          isLoading={this.props.donations.isLoading}
+          errMess={this.props.donations.errMess}
+          comments={this.props.comments.filter(
+            (comment) =>
+              comment.donationId === parseInt(match.params.donationId, 10)
+          )}
+          addComment={this.props.addComment}
+        />
       );
     };
 
@@ -79,17 +77,20 @@ class Main extends Component {
       <div>
         <Header />
         <div>
-          
           <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/aboutUs' component={AboutUs} />
-              <Route exact path='/menu' component={() => <Menu donations={this.props.donations} />} />
-              <Route path='/menu/:donationId' component={DonationWithId} />
-              <Route exact path='/contactus' component={Contact} />
-              <Route exact path='/Login' component={Login} />
-              <Route exact path='/Donate' component={Donate} />
+            <Route path="/home" component={HomePage} />
+            <Route exact path="/aboutUs" component={AboutUs} />
+            <Route
+              exact
+              path="/menu"
+              component={() => <Menu donations={this.props.donations} />}
+            />
+            <Route path="/menu/:donationId" component={DonationWithId} />
+            <Route exact path="/contactus" component={Contact} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/Donate" component={Donate} />
 
-              <Redirect to="/home" />
+            <Redirect to="/home" />
           </Switch>
         </div>
         <Footer />
