0c3d693b14f49473dabcdfa90739c2720883eb30
charbelaoun2 <64602534+charbelaoun2@users.noreply.github.com>
2022-01-08 11:55:26 +0200

    heroku link

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,3 @@
-export const baseUrl = 'http://localhost:3000/'; 
+// export const baseUrl = 'http://localhost:3000/'; 
+
+const baseUrl=(process.env.NODE_ENV === 'production' ? 'https://donationbackendwebsite.herokuapp.com/':'http://localhost:3000/')
