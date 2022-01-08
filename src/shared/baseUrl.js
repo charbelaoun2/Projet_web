408c6637d18cc027902408de9c313ddad2c2c1ea
Charbel Aoun <64602534+charbelaoun2@users.noreply.github.com>
2022-01-08 23:26:20 +0200

    Update baseUrl.js

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:3000/'; 
+const baseUrl=(process.env.NODE_ENV === 'production' ?  'https://donationbackendwebsite.herokuapp.com/' :  'http://localhost:3000/' )
